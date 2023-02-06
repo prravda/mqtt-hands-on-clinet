dcd7436e1f0cd108bc0abeb1e725e132e41dc39d
prravda <sonofhuman20@hufs.ac.kr>
2023-02-06 18:01:56 +0900

    implementation: update code from event emitter style to async/await format using `async-mqtt` for better readability

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-import * as mqtt from "mqtt"
+import * as mqtt from "async-mqtt"
 const client  = mqtt.connect('mqtt://test.mosquitto.org')
 
-client.on('connect', function () {
-    console.log('client connect: start');
-    client.subscribe('presence', function (err) {
-        if (!err) {
-            client.publish('presence', 'Hello mqtt')
-        }
-    })
-})
+const handlingMessage = async () => {
+    try {
+        await client.on('message', (topic, message) => {
+            // the message is getting into the buffer type
+            // so should convert this data into string using .toString method
+            console.log(`client get message: ${message.toString()}`);
+        });
+    } catch (e) {
+        console.log(e.stack);
+    }
+};
 
-client.on('message', function (topic, message) {
-    // message is Buffer
-    console.log();
-    console.log(`client get message: ${message.toString()}`);
-    client.end()
-})
+const run = async () => {
+    console.log("Starting the connection...");
+
+    try {
+        await client.subscribe("presence");
+        await client.publish("presence", "MQTT connection is started!");
+    } catch (e) {
+        console.log(e.stack);
+    }
+}
+
+const bootstrap = async () => {
+    await run();
+    await handlingMessage();
+}
+
+bootstrap();
